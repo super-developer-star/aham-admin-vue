e5b48e3fff4a90abe5effaabbab6579c7c1cddaa
Revaz <revaztsiklauri@gmail.com>
2019-01-11 12:05:37 +0400

    drag/drop treeview logic completed

--- src/views/ManageContent/Catalogue.vue
+++ src/views/ManageContent/Catalogue.vue
@@ -1,21 +1,31 @@
 <template>
-  <DraggableTree class="tree" :data="treeData" draggable cross-tree>
-    <div slot-scope="{data, store}">
-      <template v-if="!data.isDragPlaceHolder">
-        <v-icon v-if="data.children && data.children.length" @click="store.toggleOpen(data)">
-          {{data.open ? 'arrow_drop_down' : 'arrow_right'}}
-        </v-icon>
-        <div class="tree-node-name flex" v-bind:class="{'pl-4': !(data.children && data.children.length)}">{{data.name}}</div>
-        <v-icon class="right">add</v-icon>
-        <v-icon class="right">edit</v-icon>
-        <v-icon class="text-xs-right">delete</v-icon>
-      </template>
-    </div>
-  </DraggableTree>
+  <v-card class="pa-4">
+    <DraggableTree class="tree" :data="treeData" draggable ref="tree" @drag="onDrag" @drop="onDrop">
+      <div slot-scope="{data, store}">
+        <template v-if="!data.isDragPlaceHolder">
+          <v-icon v-if="data.children && data.children.length" @click="store.toggleOpen(data)">
+            {{data.open ? 'arrow_drop_down' : 'arrow_right'}}
+          </v-icon>
+          <div class="tree-node-name flex"
+               v-bind:class="{'pl-4': !(data.children && data.children.length),
+               'tree-category': data._vm.level === 1,
+                'tree-subject': data._vm.level === 2,
+                 'tree-sub-category': data._vm.level === 3,
+                  'tree-topic': data._vm.level === 4}">
+            {{data.name}} - {{data.draggable}}
+          </div>
+          <v-icon class="right">add</v-icon>
+          <v-icon class="right">edit</v-icon>
+          <v-icon class="text-xs-right">delete</v-icon>
+        </template>
+      </div>
+    </DraggableTree>
+  </v-card>
 </template>
 
 <script>
 import { DraggableTree } from 'vue-draggable-nested-tree'
+import * as th from 'tree-helper'
 
 export default {
   name: 'Catalogue',
@@ -35,13 +45,11 @@ export default {
               children: [
                 {
                   name: 'motion',
-                  hasChild: true,
-                  children: [{ name: 'color1', hasChild: false }]
+                  hasChild: false
                 },
                 {
                   name: 'SUB-CATEGORY',
-                  hasChild: true,
-                  children: [{ name: 'color', hasChild: false }]
+                  hasChild: false
                 }]
             },
             { name: 'Linear motion 2', hasChild: false },
@@ -58,25 +66,49 @@ export default {
         { name: 'Physics - Intermediate', hasChild: false },
         { name: 'Biology', hasChild: false }
       ]
-    }]
+    }, { name: 'Biology', hasChild: false }],
+    maxLevel: 4
   }),
+  mounted () {
+    this.setTree(this.treeData)
+  },
   methods: {
+    setTree (treeData) {
+      th.depthFirstSearch(treeData, (childNode) => {
+        this.$set(childNode, 'draggable', childNode._vm.level > 2)
+      })
+    },
     addChild (item) {
       this.treeData[0].children.push({ text: 'child' })
     },
     deleteChild (item) {
       this.treeData[0].children.push({ text: 'child' })
+    },
+    onDrag (node) {
+      let nodeLevels = 1
+      th.depthFirstSearch(node, (childNode) => {
+        if (childNode._vm.level > nodeLevels) {
+          nodeLevels = childNode._vm.level
+        }
+      })
+      nodeLevels = nodeLevels - node._vm.level + 1
+      console.log(nodeLevels)
+      const childNodeMaxLevel = this.maxLevel - nodeLevels
+
+      th.depthFirstSearch(this.treeData, (childNode) => {
+        this.$set(childNode, 'droppable', childNode._vm.level <= childNodeMaxLevel)
+      })
+    },
+    onDrop (node) {
+      console.log(node._vm.level)
+      // this.$set(node, 'draggable', node._vm.level > 2)
     }
   }
 }
 </script>
 
 <style lang="scss">
-.he-tree{
-  border: 1px solid #ccc;
-  padding: 20px;
-}
-.draggable-placeholder-inner{
+.draggable-placeholder-inner {
   border: 1px dashed #0088F8;
   box-sizing: border-box;
   background: rgba(0, 136, 249, 0.09);
@@ -86,22 +118,41 @@ export default {
   display: flex;
   align-items: center;
 }
-.tree{
-  .tree-node-inner{
+
+.tree {
+  .tree-node-inner {
     border: none;
     padding: 0;
     cursor: pointer;
+
     & > div {
       align-items: center;
       display: flex;
     }
   }
-  .tree-node-inner-back:hover{
+
+  .tree-node-inner-back:hover {
     background: #ddd;
   }
 
-  .tree-node-name{
+  .tree-node-name {
     font-size: 1.2em;
+  }
+
+  .tree-category {
+    color: red;
+  }
+
+  .tree-subject {
+    color: blue;
+  }
+
+  .tree-sub-category {
+    color: darkmagenta;
+  }
+
+  .tree-topic {
+    color: seagreen;
   }
 }
 </style>
